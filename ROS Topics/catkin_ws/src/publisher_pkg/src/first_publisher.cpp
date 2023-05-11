402d37c75b3f99ae13990271e0e25a445f7aca9e
sergio-contente <sergio.mcontente@gmail.com>
2023-05-11 19:05:53 -0300

    🚧 Commenting code and Readme

--- ROS Topics/catkin_ws/src/publisher_pkg/src/first_publisher.cpp
+++ ROS Topics/catkin_ws/src/publisher_pkg/src/first_publisher.cpp
@@ -6,18 +6,18 @@ int main(int argc, char** argv) {
     ros::init(argc, argv, "topic_publisher");
     ros::NodeHandle nh;
     
-    ros::Publisher pub = nh.advertise<std_msgs::Int32>("counter", 1000);
+    ros::Publisher pub = nh.advertise<std_msgs::Int32>("counter", 1000); //Instanciação de um publisher chamado "pub" que ira publicar mensagens do tipo std_msgs::Int32 no tópico "/counter"
     ros::Rate loop_rate(2);
     
-    std_msgs::Int32 count;
-    count.data = 0;
+    std_msgs::Int32 count; //Intanciação de uma variável "count" do tipo "std_mgs::Int32". Ela é comumente chamada de mensagem também.
+    count.data = 0; //Atribuição do valor inicial 0 ao atributo data da mensagem.
     
     while (ros::ok())
     {
-        pub.publish(count);
+        pub.publish(count); //Publicação da mensagem "count" no tópico onde o pub foi configurado para publicar (tópico "counter")
         ros::spinOnce();
         loop_rate.sleep();
-        ++count.data;
+        ++count.data; //Incrementamos em 1 o valor da mensagem
     }
     
     return 0;

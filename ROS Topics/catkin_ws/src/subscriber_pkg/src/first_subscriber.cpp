e956236f29190fbf7fbae20886fd35a724fc0274
sergio-contente <sergio.mcontente@gmail.com>
2023-05-11 23:43:19 -0300

    🚀  Finishing the module

--- ROS Topics/catkin_ws/src/subscriber_pkg/src/first_subscriber.cpp
+++ ROS Topics/catkin_ws/src/subscriber_pkg/src/first_subscriber.cpp
@@ -1,9 +1,10 @@
 #include <ros/ros.h>
-#include <std_msgs/Int32.h>
+#include <string>
+#include <subscriber_pkg/Pessoa.h>
 
-void counterCallback(const std_msgs::Int32::ConstPtr& msg) // Define uma função de callback  que recebe um parâmetro chamado "msg" 
+void counterCallback(const subscriber_pkg::Pessoa::ConstPtr& msg) // Define uma função de callback  que recebe um parâmetro chamado "msg" 
 {
-  ROS_INFO("%d", msg->data); // Printa o valor do atributo "data" do parâmetro "msg".
+  ROS_INFO("Oi, meu nome eh %s, tenho %d anos e possuo %f metros", msg->Nome.c_str(), msg->Idade, msg->Altura); // Printa o valor do atributo "data" do parâmetro "msg".
 }
 
 int main(int argc, char** argv) {
@@ -11,7 +12,7 @@ int main(int argc, char** argv) {
     ros::init(argc, argv, "topic_subscriber"); // Inicia o Node chamado "topic_subscriber" 
     ros::NodeHandle nh;
     
-    ros::Subscriber sub = nh.subscribe("counter", 1000, counterCallback); // Cria um objeto Subscriber que vai escutar ao tópico /counter e chamar o callback "counterCallback" a cada loop.    
+    ros::Subscriber sub = nh.subscribe("pessoa", 1000, counterCallback); // Cria um objeto Subscriber que vai escutar ao tópico /counter e chamar o callback "counterCallback" a cada loop.    
     ros::spin(); // Cria o loop
     
     return 0;
